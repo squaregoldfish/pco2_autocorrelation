59532a47098b0d5fa0e95a4fe5119ff451acb4f6
squaregoldfish <steve@squaregoldfish.co.uk>
2017-12-16 16:52:55 +0100

    Parallelised version
    Does not make plots - those will go in a different script

--- spatial/make_dataset_variograms.jl
+++ spatial/make_dataset_variograms.jl
@@ -1,23 +1,17 @@
 thisdir = dirname(@__FILE__())
 any(path -> path==thisdir, LOAD_PATH) || push!(LOAD_PATH, thisdir)
 using Variogram
-using Plots
-gr()
 
 const INDIR = ARGS[1]
 const OUTDIR = ARGS[2]
 
-for file in readdir(INDIR)
-
-    datasetname = file[1:end - 4]
-    dataset = readdlm("$INDIR/$file", '\t', Float64, '\n', header=true)[1]
-    variogram = Variogram.variogram(dataset, 25)
-    writedlm("$OUTDIR/$datasetname.csv", variogram, ',')
-    scatter(variogram[:,1], variogram[:,2], size=(1200,800))
-    savefig("$OUTDIR/$datasetname.png")
+@everywhere function makevariogram(indir, outdir, file)
+    local datasetname = file[1:end - 4]
+    local dataset = readdlm("$indir/$file", '\t', Float64, '\n', header=true)[1]
+    local variogram = Variogram.variogram(dataset, 25)
+    writedlm("$outdir/$datasetname.csv", variogram, ',')
 end
 
+files = readdir(INDIR)[1:15]
 
-
-
-
+@time pmap(files -> makevariogram(INDIR, OUTDIR, files), files)
