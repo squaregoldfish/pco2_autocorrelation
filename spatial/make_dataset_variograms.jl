6353cb89463a5a06a375d86dec7bfbcc326a5d71
squaregoldfish <steve@squaregoldfish.co.uk>
2017-12-24 22:13:11 +0100

    Plot variograms

--- spatial/make_dataset_variograms.jl
+++ spatial/make_dataset_variograms.jl
@@ -1,6 +1,8 @@
 thisdir = dirname(@__FILE__())
 any(path -> path==thisdir, LOAD_PATH) || push!(LOAD_PATH, thisdir)
 using Variogram
+using Plots
+gr()
 
 const INDIR = ARGS[1]
 const OUTDIR = ARGS[2]
@@ -9,8 +11,10 @@ const OUTDIR = ARGS[2]
     local datasetname = file[1:end - 4]
     print("$datasetname\n")
     local dataset = readdlm("$indir/$file", '\t', Float64, '\n', header=true)[1]
-    @time local variogram = Variogram.variogram(dataset, 25)
+    @time local variogram = Variogram.variogram(dataset, 5)
     writedlm("$outdir/$datasetname.csv", variogram, ',')
+    Plots.scatter(variogram[:,1], variogram[:,2], size=(1200,800))
+    Plots.png("$outdir/$datasetname.png")
 end
 
 files = readdir(INDIR)
