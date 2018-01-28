f26bb65654a33b2baf94a85ddc04bb75cd051ebd
squaregoldfish <steve@squaregoldfish.co.uk>
2018-01-28 18:39:00 +0100

    Do not plot

--- spatial/make_dataset_variograms.jl
+++ spatial/make_dataset_variograms.jl
@@ -1,8 +1,8 @@
 thisdir = dirname(@__FILE__())
 any(path -> path==thisdir, LOAD_PATH) || push!(LOAD_PATH, thisdir)
 using Variogram
-using Plots
-gr()
+#using Plots
+#gr()
 
 const INDIR = ARGS[1]
 const OUTDIR = ARGS[2]
@@ -39,7 +39,7 @@ const OUTDIR = ARGS[2]
         #writedlm("$outdir/$datasetname.polyfit.csv", polyfit, ',')
 
         # Plot the variogram and fitted curve
-        Plots.scatter(variogram[:,1], variogram[:,2])
+        #Plots.scatter(variogram[:,1], variogram[:,2])
 
         #model(x, p) = p[1]*(1 - exp.(-x./p[2]))
         #Plots.plot!(variogram[:,1], model(variogram[:,1], fit))
@@ -47,7 +47,7 @@ const OUTDIR = ARGS[2]
         #polymodel(x, p) = p[1] + p[2]x + p[3]x.^2 + p[4]x.^3 + p[5]x.^4 + p[6]x.^5 + p[7]x.^6 + p[8]x.^7 + p[9]x.^8 + p[10]x.^9 + p[11]x.^10
         #Plots.plot!(variogram[:,1], polymodel(variogram[:,1], polyfit))
 
-        Plots.png("$outdir/$datasetname.png")
+        #Plots.png("$outdir/$datasetname.png")
     end
 end
 
