aa8ae20ec3e75fa160c1186737172dc97b4d6c74
Steve Jones <steve@squaregoldfish.co.uk>
2018-04-21 10:31:42 +0200

    Disable plot generation

--- spatial/make_dataset_variograms.jl
+++ spatial/make_dataset_variograms.jl
@@ -1,8 +1,8 @@
 thisdir = dirname(@__FILE__())
 any(path -> path==thisdir, LOAD_PATH) || push!(LOAD_PATH, thisdir)
 using Variogram
-using Plots
-gr()
+#using Plots
+#gr()
 
 const INDIR = ARGS[1]
 const OUTDIR = ARGS[2]
@@ -25,15 +25,15 @@ const OUTDIR = ARGS[2]
         local variogram = Variogram.variogram(dataset, 25)
         writedlm("$outdir/$datasetname.csv", variogram, ',')
 
-        Plots.scatter(variogram[:,1], variogram[:,2])
+        #Plots.scatter(variogram[:,1], variogram[:,2])
 
         # Get the decorrelation length of the variogram
         decorrelationlength = Variogram.getdecorrelationlength(variogram, true)
 
-        plotmarker = [[decorrelationlength, decorrelationlength] [0, maximum(variogram[:,2])]]
-        Plots.plot!(plotmarker[:,1], plotmarker[:,2], linewidth=1, color="red", linestyle=:dot)
+        #plotmarker = [[decorrelationlength, decorrelationlength] [0, maximum(variogram[:,2])]]
+        #Plots.plot!(plotmarker[:,1], plotmarker[:,2], linewidth=1, color="red", linestyle=:dot)
 
-        Plots.png(plotfile)
+        #Plots.png(plotfile)
     catch ex
         println(typeof(ex))
         showerror(STDOUT, ex)
